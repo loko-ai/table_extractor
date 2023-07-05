2482e15ea43e77cf3c28a6432edfd7eec6b046aa
robertaparisi <roberta.parisi2@gmail.com>
2023-07-05 19:04:40 +0200

    mfix on non mr doc; handled errors; update requirements

--- business/non_mr_extraction.py
+++ business/non_mr_extraction.py
@@ -3,6 +3,7 @@
 # import table_ocr.extract_cells
 # import table_ocr.ocr_image
 # import table_ocr.ocr_to_csv
+from fastapi import HTTPException
 from img2table.document import PDF, Image
 from img2table.ocr import TesseractOCR
 from loguru import logger
@@ -40,14 +41,30 @@ from loguru import logger
 
 
 def extraxt_table_from_non_mr_img(doc_filepath, ext_type="pdf", non_mr_kwargs=None):
+    logger.debug("Extracting table from non mr img/pdf")
     ocr = TesseractOCR(**non_mr_kwargs["ocr_kwargs"])
-    if ext_type == "pdf":
-        doc = PDF(src=doc_filepath)
+    logger.debug(f"Extension type {ext_type}, Doc file path {doc_filepath}")
+    if ext_type == ".pdf":
+        logger.debug("opening pdf...")
+        doc = PDF(src=doc_filepath, pdf_text_extraction=False)
     else:
+        logger.debug("opening image...")
         # Instantiation of the image
         doc = Image(src=doc_filepath)
+    logger.debug(f"going to extract from doc::: {doc} ")
+
 
     # Table identification
     doc_tables = doc.extract_tables(ocr=ocr, **non_mr_kwargs["extraction_kwargs"])
-    res = [table.df.to_dict(orient="records") for table in doc_tables]
+    logger.debug(f"extracted doc tables types and content {type(doc_tables), doc_tables}")
+    if isinstance(doc_tables, list):
+        res = [table.df.to_dict(orient="records") for table in doc_tables]
+    elif isinstance(doc_tables, dict):
+        res = {idx:[table.df.to_dict(orient="records") for table in table_list] for idx, table_list in doc_tables.items()}
+    else:
+        msg = "ERROR!!! The extracted table has a type not yet supported!"
+        logger.error(msg)
+        raise Exception(msg)
+        # raise HTTPException(detail=msg, status_code=400)
+    logger.debug("ciaon")
     return res
--- services/services.py
+++ services/services.py
@@ -3,10 +3,13 @@ import pathlib
 import re
 
 import uvicorn as uvicorn
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
 from loguru import logger
 from loko_extensions.business.decorators import extract_value_args
 
-from fastapi import Request, FastAPI, File
+from fastapi import Request, FastAPI, File, HTTPException, UploadFile
+from starlette.responses import JSONResponse, PlainTextResponse
 
 from business.table_extraction import get_file_extension, extract_table
 
@@ -16,10 +19,25 @@ from utils.decorator_fastAPI import ExtractValueArgsFastapi
 
 app = FastAPI()
 
+#
+# @app.exception_handler(RequestValidationError)
+# async def validation_exception_handler(request, exc):
+#     return PlainTextResponse(str(exc), status_code=400)
+
+@app.exception_handler(Exception)
+async def handle_exception(request, exc):
+    # Log the exception
+    logger.error(str(exc))
+
+    # Propagate the exception as a JSON response
+    response_data = {"detail": str(exc)}
+    response_json = jsonable_encoder(response_data)
+    return JSONResponse(status_code=500, content=response_json)
 
 @app.post("/extract_table")
 @ExtractValueArgsFastapi(file=True)
 def extract_table_ocr(file, args):
+    # logger.debug("AAAAAAAAAAAA"*1000)
     logger.debug(f"file {file.filename}")
     logger.debug(f"args {args}")
     logger.debug(f"body:: {type(file)}")
@@ -37,11 +55,48 @@ def extract_table_ocr(file, args):
     non_mr_kwargs["extraction_kwargs"] = dict()
     non_mr_kwargs["extraction_kwargs"]["implicit_rows"] = args.get("implicit_rows", True)
     non_mr_kwargs["extraction_kwargs"]["borderless_tables"] = args.get("borderless_tables", False)
-
-    res = extract_table(file, type_of_doc, camelot_kwargs, non_mr_kwargs)
+    try:
+        res = extract_table(file, type_of_doc, camelot_kwargs, non_mr_kwargs)
+    except Exception as inst:
+        logger.error("error extracting table: ",inst)
+        raise Exception(inst)    # except Exception as inst:
 
     logger.debug(f"resssssssssssss {res}")
     return dict(content=res)
+
+
+
+@app.post("/extract_table_basic")
+def extract_table_ocr(file: UploadFile= File()):
+    args= {'id': '0d973f8e-fb66-47a4-8d94-20e128b07c55', 'name': 'TableExtractor', 'headers': {}, 'comment': '', 'alias': '',
+     'debug': True, 'type_of_doc': 'Non Machine-Readable', 'lang': 'ita', 'borderless_tables': False,
+     'implicit_rows': False}
+    # logger.debug("AAAAAAAAAAAA"*1000)
+    logger.debug(f"file {file.filename}")
+    logger.debug(f"args {args}")
+    logger.debug(f"body:: {type(file)}")
+
+    type_of_doc = args.pop("type_of_doc")
+    camelot_kwargs = dict()
+    camelot_kwargs["process_background"] = args.get("process_background", False)
+    camelot_kwargs["flavor"] = args.get("flavor", "lattice")
+    camelot_kwargs["line_scale"] = int(args.get("line_scale", 15))
+    camelot_kwargs["split_text"] = args.get("split_text", False)
+    logger.debug(f"=================== CAMELOT {camelot_kwargs}")
+    non_mr_kwargs = dict()
+    non_mr_kwargs["ocr_kwargs"] = dict()
+    non_mr_kwargs["ocr_kwargs"]["lang"] = args.get("lang", "ita")
+    non_mr_kwargs["extraction_kwargs"] = dict()
+    non_mr_kwargs["extraction_kwargs"]["implicit_rows"] = args.get("implicit_rows", True)
+    non_mr_kwargs["extraction_kwargs"]["borderless_tables"] = args.get("borderless_tables", False)
+    try:
+        res = extract_table(file, type_of_doc, camelot_kwargs, non_mr_kwargs)
+    except Exception as inst:
+        logger.error("error extracting table: ",inst)
+        raise Exception(inst)
+    logger.debug(f"resssssssssssss {res}")
+    return dict(content=res)
+
 
 
 #
